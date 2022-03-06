a076b4f099aa05f3084289abebc2f3d1f621f454
Lara <laraellenh@gmail.com>
2022-03-06 14:06:14 -0500

    commit tree e3dfe746533aae7d099a80453759ba27de1784ab

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+dwarves =  %w[
+  Doc
+  Dopey
+  Sleepy
+  Bashful
+  Grumpy
+]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each.with_index do |d, index|
+  puts "#{index + 1}. #{d}"
+  end
 end
+# .map iterates and returns new array with updated values, .capitalize is a method capitalizing first letter of each element
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map {|call| call.capitalize + "!"}
 end
-
-def find_the_cheese# code an argument here
+# invoke the method using the array of planateers as argument
+summon_captain_planet(planeteer_calls)
+# if there are any (use any method) with length greater than 4 characters, returns true
+def long_planeteer_calls planeteer_calls
+  planeteer_calls.any? {|p|p.length > 4}
+end
+# make variable for the array
+snacksarray =  ["potato", "hamsterfood", "gouda", "canned meats"]
+def find_the_cheese snacksarray
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # use .find method on the array 
+  snacksarray.find do |cheese|
+    cheese_types.include? cheese
+  end
 end
+# invoke with the array as argument
+find_the_cheese snacksarray
